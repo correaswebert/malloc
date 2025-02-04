e4e08bf906d3928109a9c95b085691402eb1f502
Swebert Correa <correaswebert@gmail.com>
2025-02-04 12:20:33 -0500

    Add test cases

--- test/malloc.c
+++ test/malloc.c
@@ -1,13 +1,43 @@
 #include <assert.h>
 
 #include "malloc.h"
+#include "test_malloc.h"
 
 int
 main()
 {
     assert(fsm_algo == FSM_FIRST_FIT);
 
-    void *ptr = malloc(10);
+    malloc_print();
+    void *p1 = malloc(10);
+    malloc_print();
+    free(p1);
+    malloc_print();
+
+    void *p2 = malloc(10);
+    malloc_print();
+    void *p3 = malloc(10);
+    malloc_print();
+    void *p4 = malloc(10);
+    malloc_print();
+    void *p5 = malloc(10);
+    malloc_print();
+    void *p6 = malloc(10);
+    malloc_print();
+
+    free(p3);
+    malloc_print();
+
+    free(p5);
+    malloc_print();
+
+    free(p2); // FIX: issue occurring here
+    malloc_print();
+
+    free(p4);
+    free(p6);
+
+    malloc_print();
 
     return 0;
 }
