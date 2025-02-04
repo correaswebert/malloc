a814eefb0057107c56f793f1573e5e8a3e077a9b
Swebert Correa <correaswebert@gmail.com>
2025-02-04 02:34:21 -0500

    Add logic around size and blocks

--- src/malloc.c
+++ src/malloc.c
@@ -6,33 +6,42 @@
 #define MEM_BLOCK_NAME_SIZE 8
 #define PAGE_SIZE 4096
 
-static void *initial_break = NULL;
+static_assert(sizeof(void *) % 8 == 0, "Pointer size is not a multiple of 8");
+
+#define BLOCK_SIZE_FLAG_MASK 0b111
+#define BLOCK_FREE 1
+#define BLOCK_PAGE_ALIGNED 1 << 1
+
 static void *current_break = NULL;
 
 typedef struct mem_block {
     // optional for debugging
     char name[MEM_BLOCK_NAME_SIZE];
 
-    // Byte 0: 0 = free, 1 = used
-    // If free then the size is used as a free list pointer
+    // blocks are allocated in word sizes, which are generally greater than 8 bytes
+    // so the last bit is always zero, and can be used as a flag
+    // Let Byte 0 indicate LSB
+    // Byte 0: 1 -> free, used to get next free block
+    // Byte 1: 1 -> page aligned, used to sbrk the memory back
     size_t size;
 
     struct mem_block *next_block;
     struct mem_block *prev_block;
-
-    void *data;
 } mem_block_t;
+
+mem_block_t *free_list_head = NULL;
+mem_block_t *block_list_head = NULL;
+
+mem_block_t *
+get_header(void *ptr)
+{
+    return (mem_block_t *)((char *)ptr - sizeof(mem_block_t));
+}
 
 static inline size_t
 align(size_t n, int align)
 {
     return (n + align - 1) & ~(align - 1);
-}
-
-void
-init_malloc()
-{
-    initial_break = sbrk(0);
 }
 
 bool
@@ -47,16 +56,34 @@ extend_memory(size_t size)
 
     current_break = new_break;
 
+    block_list_head->size += size;
+
     return true;
 }
 
-void *
-get_free_block(size_t size)
+bool
+release_memory(size_t size)
 {
-    struct mem_block *block = current_break;
+    assert(size % PAGE_SIZE == 0);
+
+    void *new_break = sbrk(-size);
+    if (new_break == (void *)-1) {
+        return false;
+    }
+
+    current_break = new_break;
+
+    return true;
+
+}
+
+mem_block_t *
+get_first_fit(size_t size)
+{
+    mem_block_t *block = free_list_head;
 
     while (block) {
-        if (block->size >= size) {
+        if ((block->size & ~BLOCK_SIZE_FLAG_MASK) >= size) {
             return block;
         }
 
@@ -66,10 +93,45 @@ get_free_block(size_t size)
     return NULL;
 }
 
-static inline void
-mark_as_used_free_block(mem_block_t *block)
+/**
+ * @param size memory size to be allocated
+ * @return pointer to the allocated memory
+ */
+static inline mem_block_t *
+get_free_block(size_t size)
 {
-    block->size = block->size & ~1;
+    switch (fsm_algo)
+    {
+    case FSM_FIRST_FIT:
+        return get_first_fit(size);
+    
+    default:
+        assert(!"Invalid FSM algorithm");
+    }
+}
+
+static inline void
+mark_block_used(mem_block_t *block)
+{
+    block->size &= ~BLOCK_FREE;
+}
+
+static inline void
+mark_block_free(mem_block_t *block)
+{
+    block->size |= BLOCK_FREE;
+}
+
+static inline bool
+is_block_free(mem_block_t *block)
+{
+    return block->size & BLOCK_FREE;
+}
+
+static inline bool
+is_block_page_aligned(mem_block_t *block)
+{
+    return block->size & BLOCK_PAGE_ALIGNED;
 }
 
 void *
@@ -77,7 +139,7 @@ malloc(size_t size)
 {
     mem_block_t *block = NULL;
 
-    size = align(size, sizeof(void *));
+    size = align(size + sizeof(mem_block_t), sizeof(void *));
 
     block = get_free_block(size);
 
@@ -92,19 +154,41 @@ malloc(size_t size)
         block = get_free_block(size);
     }
 
-    mark_as_used_free_block(block);
+    mark_block_used(block);
 
-    return block->data;
+    return block + sizeof(mem_block_t);
+}
+
+static inline void
+merge_neigh_free_blocks(mem_block_t *block)
+{
+    mem_block_t *next_block = block->next_block;
+
+    if (next_block && is_block_free(next_block)) {
+        block->size += next_block->size;
+        block->next_block = next_block->next_block;
+    }
+
+    mem_block_t *prev_block = block->prev_block;
+
+    if (prev_block && is_block_free(prev_block)) {
+        prev_block->size += block->size;
+        prev_block->next_block = block->next_block;
+    }
 }
 
 void
 free(void *ptr)
 {
-    // mark block as free
+    mem_block_t *block = get_header(ptr);
 
-    // combine with the previous and next free blocks
+    mark_block_free(block);
 
-    // sbrk the memory back if possible
+    merge_neigh_free_blocks(block);
+
+    if (is_block_page_aligned(block)) {
+        release_memory(block->size);
+    }
 }
 
 bool
